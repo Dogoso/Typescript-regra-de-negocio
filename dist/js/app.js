eae27d34cc9e1801377ed9343d3343d9e17e94a6
Silas Lago <juanfsa@live.com>
2022-11-20 12:37:07 -0300

    Negociacoes class created

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,7 @@
-import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 10, 100);
-alert(negociacao.getVolume);
-alert(negociacao.getQuantidade);
-alert(negociacao.getValor);
-alert(negociacao.getData);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const negociacao = new NegociacaoController();
+const form = document.querySelector(".form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    negociacao.adicionar();
+});
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,38 +1,28 @@
 import { Negociacao } from "../models/negociacao";
 import { Negociacoes } from "../models/negociacoes";
-
 export class NegociacaoController {
-
-    private inputData: HTMLInputElement;
-    private inputQuantidade: HTMLInputElement;
-    private inputValor: HTMLInputElement;
-    private negociacoes: Negociacoes = new Negociacoes();
-
     constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector("#quantidade");
         this.inputValor = document.querySelector("#valor");
     }
-
-    adicionar(): void {
+    adicionar() {
         const negociacao = this.negociacaoFactory();
-        this.negociacoes.adiciona(negociacao)
+        this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
     }
-
-    negociacaoFactory(): Negociacao {
+    negociacaoFactory() {
         const rex = /-/g;
         const curDate = new Date(this.inputData.value.replace(rex, ","));
         const quantity = parseInt(this.inputQuantidade.value);
         const value = parseFloat(this.inputValor.value);
         return new Negociacao(curDate, quantity, value);
     }
-
-    limparFormulario(): void {
+    limparFormulario() {
         this.inputData.value = "";
         this.inputQuantidade.value = "";
         this.inputValor.value = "";
         this.inputData.focus();
     }
-
 }
